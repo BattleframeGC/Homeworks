1888b15ee0c3fd1455bb7be7a1166bead7175cc5
BattleFrame <battleframe.gc@yandex.ru>
2021-11-22 22:10:57 +0300

    Добавлено задание 10.2

---
+++ [2] javascript/Задание 2.10/task_2/js/hw-10.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function functionClick(event) 
+{
+	let target = event.target;
+  let count = this.getAttribute('count');
+
+	if (target.tagName != 'BUTTON') return;
+			
+	let className = target.className;
+	let type = (className === "plus") ? target.previousElementSibling : target.nextElementSibling;
+  let int = parseInt(type.innerText);
+  
+  if (int > 0     && className === "minus")   type.innerText--;
+  if (int < count && className === "plus")    type.innerText++;
+}
+
+
+const countInfo = document.querySelectorAll('.card__count-info');
+  console.log('click');
+
+for (let counter of countInfo) {
+	counter.addEventListener('click', functionClick);
+}
