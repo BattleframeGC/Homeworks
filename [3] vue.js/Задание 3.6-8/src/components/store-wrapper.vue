a566d4be9ea4f647f91fb2acfb474c49519ca7c5
BattleFrame <battleframe.gc@yandex.ru>
2021-12-26 17:04:32 +0300

    ++ Задание 3.6-8 (VUE-CLI) Финальная версия

---
+++ [3] vue.js/Задание 3.6-8/src/components/store-wrapper.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="store-wrapper">
+      <router-view></router-view>
+    </div>
+
+</template>
+
+<script>
+
+
+    export default {
+        name: 'store-wrapper',
+        data() {
+             return {}
+        }
+     }
+</script>
+
+
+<style>
+  .store-wrapper {
+    margin: 0 auto;
+  }
+</style>
